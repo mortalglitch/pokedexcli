ccefdb4a5053a3ae94194c0b5969b6b0721a7150
mortalglitch <mortal.glitch@gmail.com>
2025-12-01 14:36:24 -0500

    Catching up to the lesson by splitting the package and adding the config functionality.

--- command_map.go
+++ command_map.go
@@ -1,34 +1,41 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func getCurrentMap() error {
-	if CurrentMapAreaList.Next == ""{
-		startMapData()
-	} else {
-		nextMapData()
+func commandMapf(cfg *config) error {
+	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
+	if err != nil {
+		return err
 	}
 
-	for _, i := range CurrentMapAreaList.List {
-		fmt.Println(i.Name)
-	}
+	cfg.nextLocationsURL = locationsResp.Next
+	cfg.prevLocationsURL = locationsResp.Previous
 
+	for _, loc := range locationsResp.Results {
+		fmt.Println(loc.Name)
+	}
 	return nil
 }
 
-func getPreviousMap() error {
-	if CurrentMapAreaList.Previous == ""{
-		return fmt.Errorf("No previous map data")
-	} else {
-		previousMapData()
+func commandMapb(cfg *config) error {
+	if cfg.prevLocationsURL == nil {
+		return errors.New("you're on the first page")
 	}
 
-	for _, i := range CurrentMapAreaList.List {
-		fmt.Println(i.Name)
+	locationResp, err := cfg.pokeapiClient.ListLocations(cfg.prevLocationsURL)
+	if err != nil {
+		return err
 	}
 
+	cfg.nextLocationsURL = locationResp.Next
+	cfg.prevLocationsURL = locationResp.Previous
+
+	for _, loc := range locationResp.Results {
+		fmt.Println(loc.Name)
+	}
 	return nil
 }
 
--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
+import (
+	"time"
+
+	"github.com/mortalglitch/pokedexcli/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
 
