3fadc298a4d22a1e73757aaffa29c563059d7d1a
mortalglitch <mortal.glitch@gmail.com>
2025-12-01 12:23:51 -0500

    refactored to match lesson progress.

--- main.go
+++ main.go
@@ -1,19 +1,6 @@
 package main
-import (
-	"fmt"
-	"bufio"
-	"os"
-
-)
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for ;; {
-		fmt.Print("Pokedex > ")
-		if scanner.Scan() != false{
-			cleanText := cleanInput(scanner.Text())		
-			fmt.Println("Your command was:", cleanText[0])
-		}
-	}
+	startRepl()
 }
 
