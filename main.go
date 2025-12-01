71f5283f9c4c447ab793cf953bef4bc5fb1832e6
mortalglitch <mortal.glitch@gmail.com>
2025-12-01 10:27:24 -0500

    Changed main to launch REPL functionality

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 import (
 	"fmt"
+	"bufio"
+	"os"
+
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for ;; {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() != false{
+			cleanText := cleanInput(scanner.Text())		
+			fmt.Println("Your command was:", cleanText[0])
+		}
+	}
 }
 
